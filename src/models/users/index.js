d7624d695fafbba66e9d7deff11cd62a51ef130e
Rodrigo Masid <remd884@mailfence.com>
2025-03-07 14:14:47 +0100

    addUserController & derived functions

---
+++ src/models/users/index.js
@@ -0,0 +1,4 @@
+import addUserModel from './addUserModel.js';
+import selectUserByUsernameModel from './selectUserByUsernameModel.js';
+
+export { addUserModel, selectUserByUsernameModel };
--- src/utils/generateErrorUtil.js
+++ src/utils/generateErrorUtil.js
@@ -1,7 +1,12 @@
-// Function which generates an error
-const generateErrorUtil = (msg, code) => {
+// Function to generate and throw a custom error with a message and HTTP status code
+const generateErrorUtil = (msg, code = 500) => {
+    if (typeof code !== 'number' || code < 100 || code > 599) {
+        throw new Error('Invalid HTTP status code');
+    }
+
     const err = new Error(msg);
     err.httpStatus = code;
+    err.name = 'CustomError'; // Optional custom name for better identification in debugging
     throw err;
 };
 
