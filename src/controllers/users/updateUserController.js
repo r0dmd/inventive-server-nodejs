72578dab753f5037534061fc0cc54acbe834b5b8
Rodrigo Masid <remd884@mailfence.com>
2025-04-08 13:27:55 +0200

    temp

--- src/controllers/users/updateUserController.js
+++ src/controllers/users/updateUserController.js
@@ -1,26 +1,18 @@
+import { updateUserModel } from '../../models/users/index.js';
 import { generateErrorUtil } from '../../utils/index.js';
 import bcrypt from 'bcrypt';
-// import { updateUserModel, getUserByIdModel } from '../../models/users/index.js';
-
 
 // Controller function to update the username and password
 const updateUserController = async (req, res, next) => {
     try {
-        // Get the user ID from request parameters
-        const { userId } = req.params;
+        const { userId } = req.user.id;
 
         // Get new username and password from request body
         const { username, password } = req.body;
 
         // If both fields are missing, throw an error
         if (!username && !password) {
-            throw generateErrorUtil('Missing fields', 400);
-        }
-
-        // Fetch the user to ensure they exist
-        const user = await userModel.getUserById(userId);
-        if (!user) {
-            throw generateErrorUtil('User not found', 404);
+            generateErrorUtil('Missing fields', 400);
         }
 
         // Prepare update data
@@ -29,7 +21,7 @@ const updateUserController = async (req, res, next) => {
         if (password) updatedData.password = await bcrypt.hash(password, 10);
 
         // Update the user
-        await userModel.updateUser(userId, updatedData);
+        await updateUserModel(userId, updatedData);
 
         // Send response to the client
         res.send({
