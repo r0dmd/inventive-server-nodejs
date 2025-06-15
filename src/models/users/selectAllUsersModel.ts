7cb0aa529ad0a8126e4f49d67abe80f9a344757d
Rodrigo Masid <remd884@mailfence.com>
2025-06-15 20:05:46 +0200

    getallusercontr migrated

--- src/models/users/selectAllUsersModel.ts
+++ src/models/users/selectAllUsersModel.ts
@@ -1,11 +1,12 @@
 import getPool from "../../db/getPool";
+import type { UserRows } from "../../types/user";
 
 // ------------------------------------------
-const selectAllUsersModel = async () => {
+const selectAllUsersModel = async (): Promise<UserRows> => {
   const pool = await getPool();
 
   // We only select users that have not been deleted
-  const [users] = await pool.query(
+  const [users] = await pool.query<UserRows>(
     `SELECT id, username, role, createdAt, modifiedAt FROM users WHERE password <> "Removed user"`,
   );
   return users;
